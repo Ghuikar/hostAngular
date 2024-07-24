d7adc76538fdac25b4e1125e9a0cdd35bf83e55e
Akshay <akshayghuikard.com>
2024-07-24 14:45:37 +0530

    chatbox icon

--- chatBox/src/app/app.component.ts
+++ chatBox/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'chatBox';
+
+
+  isOpen = false;
+  messages: string[] = [];
+  newMessage: string = '';
+
+  toggleChatbox() {
+    this.isOpen = !this.isOpen;
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.messages.push(this.newMessage.trim());
+      this.newMessage = '';
+    }
+  }
 }
